c597dbd2219e2e62781d4c0b08a7ab7a05e76363
Your Name <you@example.com>
2014-07-20 15:24:24 +0300

    Add two cards layouts

--- gen/com/googleglassuiwidgets/R.java
+++ gen/com/googleglassuiwidgets/R.java
@@ -10,6 +10,27 @@ package com.googleglassuiwidgets;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  The maximum height of the body content inside a card. 
+         */
+        public static final int glass_card_body_height=0x7f040003;
+        /** 
+    	 The recommended margin between the bottom of the card and the footer. This is
+    	 an adjusted value so that the baseline of the text in the footer sits 40px
+    	 from the bottom of the card, matching the other margins.
+    
+         */
+        public static final int glass_card_footer_margin=0x7f040001;
+        /**  The width of the left column in the two-column layout. 
+         */
+        public static final int glass_card_left_column_width=0x7f040004;
+        /**  The recommended margin for the top, left, and right edges of a card. 
+         */
+        public static final int glass_card_margin=0x7f040000;
+        /**  The recommended margin for the left column of the two-column card. 
+         */
+        public static final int glass_card_two_column_margin=0x7f040002;
+    }
     public static final class drawable {
         public static final int dog_1_60x60=0x7f020000;
         public static final int dog_2_60x60=0x7f020001;
@@ -17,8 +38,20 @@ public final class R {
         public static final int dogs_120x120=0x7f020003;
         public static final int ic_launcher=0x7f020004;
     }
+    public static final class id {
+        public static final int body_layout=0x7f070005;
+        public static final int footer=0x7f070002;
+        public static final int footer_container=0x7f070001;
+        public static final int layout=0x7f070004;
+        public static final int left_column=0x7f070000;
+        public static final int timestamp=0x7f070003;
+    }
+    public static final class layout {
+        public static final int left_column_layout=0x7f030000;
+        public static final int single_column_layout=0x7f030001;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
         /** 
@@ -42,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
